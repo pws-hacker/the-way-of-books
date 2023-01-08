577b6c44f1004b31800ae04ab8331c25795d01da
xu.dehao <xu.dehao@outlook.com>
2023-01-08 12:58:02 +0800

    同步文件

--- Write_Quality_Code-125_Suggestions_for_Improving_your_C_program_Code--MaWei/part11~12/src/main.cpp
+++ Write_Quality_Code-125_Suggestions_for_Improving_your_C_program_Code--MaWei/part11~12/src/main.cpp
@@ -1,5 +1,11 @@
-#include "advise97.h"
+#include "advise102~104.h"
+#include "advise105~108.h"
+
 int main()
 {
+	Advise105 advise105;
+	advise105.test_01_demo();
+	advise105.test_02_error();
+	advise105.test_02_right();
 	return 0;
 }
